0f580a2f87ca43ab6d705cc7c32ca8974205aa3e
CSC91 <angelalbertocordovaenriquez@gmail.com>
2024-07-30 21:46:12 -0600

    a

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,10 @@
-(function(){
-    
-    const sliders = [...document.querySelectorAll('.testimony__body')];
-    const buttonNext = document.querySelector('#next');
-    const buttonBefore = document.querySelector('#before');
-    let value;   
+const images = document.querySelectorAll('.gallery__image');
+let currentImage = 0;
 
-    buttonNext.addEventListener('click', ()=>{
-        changePosition(1);
-    });
+function showNextImage() {
+    images[currentImage].classList.remove('active');
+    currentImage = (currentImage + 1) % images.length;
+    images[currentImage].classList.add('active');
+}
 
-    buttonBefore.addEventListener('click', ()=>{
-        changePosition(-1);
-    });
-
-    const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number(currentTestimony);
-        value+= add;
-
-
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
-        }
-
-        sliders[value-1].classList.add('testimony__body--show');
-
-    }
-
-})();
+setInterval(showNextImage, 2000);  // Cambia de imagen cada 2 segundos
